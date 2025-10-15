3926e00364ee5dc0a81fbc3578cda46f95c846d0
ousemaakacha <akachaousema@gmail.com>
2025-10-15 19:42:14 +0200

    annulla btn addition with style to diplay ofr the ticket div and fixed the evente by adding the prevent deafault

--- js/bigliettoTreno.js
+++ js/bigliettoTreno.js
@@ -3,15 +3,18 @@ console.log("biglietto treno chuchuuu");
 // inizioa dichiarare le costanti utilizzando il getelem
 const generaBtn = document.getElementById("genera");
 const annullaBtn = document.getElementById("annulla");
+
 // aggiungo const per il div contente la tabella
 const ticketDiv = document.getElementById("ticket");
 
 // comincio a formulare il event listener per il genaraBtn
 
-generaBtn.addEventListener("click", () => {
+generaBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
   // all'interno inizio a scrivere la funzione inizializzata da =>
   const name = document.getElementById("name").value;
-  const km = document.getElementById("numberKm").value;
+  const km = document.getElementById("Km").value;
   const age = document.getElementById("age").value;
 
   //  inizio  a calcolare il prezzo base
@@ -20,10 +23,10 @@ generaBtn.addEventListener("click", () => {
 
   //   inizio a scrivere codice condizionale per gli sconti
   if (age === "minorenne") {
-    price * 0.8;
+    price *= 0.8;
     ticketType = "SConto 20% per minorenni";
   } else if (age === "over65") {
-    price * 0.6;
+    price *= 0.6;
     ticketType = "sconto 40% over 65";
   }
 
@@ -39,4 +42,11 @@ generaBtn.addEventListener("click", () => {
   document.getElementById("wagon").textContent = wagon;
   document.getElementById("codeCP").textContent = codeCP;
   document.getElementById("price").textContent = price.toFixed(2);
+
+  ticketDiv.style.display = "block";
+});
+
+annullaBtn.addEventListener("click", () => {
+  document.getElementById("ticketForm").reset();
+  ticketDiv.style.display = "none";
 });
