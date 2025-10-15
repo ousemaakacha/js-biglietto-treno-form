a2ca88789b898d7f582cd51616fdbd1158049f67
ousemaakacha <akachaousema@gmail.com>
2025-10-15 18:37:49 +0200

    added the value for the diff option into the html and wrote the coditional code for the different discounts
    added the required tag into the html for so that user is required to fill the inputs

--- js/bigliettoTreno.js
+++ js/bigliettoTreno.js
@@ -15,4 +15,13 @@ generaBtn.addEventListener("click", () => {
   //  inizio  a calcolare il prezzo base
   let price = km * 0.21;
   let ticket_type = "Biglietto standard";
+
+  //   inizio a scrivere codice condizionale per gli sconti
+  if (age === "minorenne") {
+    price * 0.8;
+    ticket_type = "SConto 20% per minorenni";
+  } else if (age === "over65") {
+    price * 0.6;
+    ticket_type = "sconto 40% over 65";
+  }
 });
