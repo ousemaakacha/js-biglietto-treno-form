2ee4baebc249707c243e174f94d168c249525482
ousemaakacha <akachaousema@gmail.com>
2025-10-15 18:14:55 +0200

    create const var for eventlistener

--- js/bigliettoTreno.js
+++ js/bigliettoTreno.js
@@ -1,1 +1,7 @@
 console.log("biglietto treno chuchuuu");
+
+// inizioa dichiarare le costanti utilizzando il getelem
+const generaBtn = document.getElementById("genera");
+const annullaBtn = document.getElementById("annulla");
+
+// comincio a formulare il event listener
