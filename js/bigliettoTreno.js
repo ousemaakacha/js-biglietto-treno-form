5cecdf698834573fe7469238bb397c9780d4b898
ousemaakacha <akachaousema@gmail.com>
2025-10-15 18:24:08 +0200

    var for the fucnction fort he genera btn event listener

--- js/bigliettoTreno.js
+++ js/bigliettoTreno.js
@@ -4,4 +4,11 @@ console.log("biglietto treno chuchuuu");
 const generaBtn = document.getElementById("genera");
 const annullaBtn = document.getElementById("annulla");
 
-// comincio a formulare il event listener
+// comincio a formulare il event listener per il genaraBtn
+
+generaBtn.addEventListener("click", () => {
+  // all'interno inizio a scrivere la funzione inizializzata da =>
+  const name = document.getElementById("name").value;
+  const km = document.getElementById("numberKm").value;
+  const age = document.getElementById("age").value;
+});
